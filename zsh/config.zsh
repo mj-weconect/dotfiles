60db95f6fe08e745ef3cdd78f3b39f5ea656b43c
Marc Jeanson <git@marcjeanson.com>
2017-01-04 00:03:46 +0100

    make ctrl-d work like I'm used to

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -20,7 +20,9 @@ setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
+
+# I like ctrl-d to close a terminal, so I'm commenting this out:
+# setopt IGNORE_EOF
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
